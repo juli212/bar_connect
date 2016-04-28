65c87dc954d954b5cbd501f0cd383ac48c5e412d
Alex Forger & john seo <pair+darrow87.seodo@devbootcamp.com>
2016-04-28 16:44:59 -0400

    event rout/controller/migration

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,27 @@
+get '/events' do
+  @events = Event.all
+  erb :'/events/index'
+end
+
+get '/events/new' do
+  erb :'/events/new'
+end
+
+post '/events' do
+  @event = Event.new(params[:event])
+  if @event.save
+    redirect '/events'
+  else
+    @errors = @event.errors.full_messages
+    erb :'/events/new'
+  end
+end
+
+get'/events/:id' do
+  @event = Event.find_by(id: params[:id])
+  erb :'/events/show'
+end
+
+get '/events/:id/edit' do
+@event = Event.find_by(id: params[:id])
+end
--- db/migrate/20160428144512_create_events.rb
+++ db/migrate/20160428144512_create_events.rb
@@ -1,6 +1,7 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+      t.string :title, null: false
       t.string :location, null: false
       t.date :time, null: false
       t.date :deadline, null: false
@@ -8,7 +9,7 @@ class CreateEvents < ActiveRecord::Migration
       t.integer :maximum, null: false
       t.references :game, null: false
 
-      t.timestamps null, false
+      t.timestamps null: false
     end
   end
 end
