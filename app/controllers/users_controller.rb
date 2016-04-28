ba98f8645d52a38c08e20e4becc54c07e09d0c54
Julia Giraldi <email": null>
2016-04-28 18:49:04 -0400

    add login/registration form, homepage styling and ajax

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+get '/users/new' do
+  erb :'/users/new'
+end
+
+get '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  if @user
+    erb :'/users/show'
+  else
+    redirect "/"
+  end
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+    erb :'/users/new'
+  end
+end
+
+get '/users' do
+  redirect '/'
+end
+
+get '/users/:id/edit' do
+  @user = User.find_by(id: params[:id])
+  erb :'/users/edit'
+end
+
+put '/users/:id' do
+  @user = User.find_by(id: params[:id])
+  @user.assign_attributes(params[:user])
+  if @user.save
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @user.errors.full_messages
+
+  end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#login a').on('click', function(event){
+    event.preventDefault();
+    $target = $(event.target)
+    $.ajax({
+      url: $target.attr('href')
+    }).done(function(response){
+        $('#form-container').html(response).hide().fadeIn(300);
+    })
+  })
 });
