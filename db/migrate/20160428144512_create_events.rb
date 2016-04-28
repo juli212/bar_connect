62df2827ad6931bd95ecec49c764349d9fd6e436
Alex Forger & john seo <pair+darrow87.seodo@devbootcamp.com>
2016-04-28 14:52:06 -0400

    add event model and migration

---
+++ db/migrate/20160428144512_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :location, null: false
+      t.date :time, null: false
+      t.date :deadline, null: false
+      t.integer :minimum, null: false
+      t.integer :maximum, null: false
+      t.references :game, null: false
+
+      t.timestamps null, false
+    end
+  end
+end
