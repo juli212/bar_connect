c69b58d5dccf89a03830e7a7d5ce0890e7927a8d
Julia Giraldi <email": null>
2016-04-29 16:20:49 -0400

    add stuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :events
+  belongs_to :event
+  belongs_to :user
 
   validates :body, presence: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
   has_many :user_events
   has_many :users, through: :user_events
+
+
+
 end
